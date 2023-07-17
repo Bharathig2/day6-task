d194ecf7cdfd8a3737b05597c0e9e401168552a7
Bharathig2 <138103694+Bharathig2@users.noreply.github.com>
2023-07-17 22:14:49 +0530

    Add files via upload

---
+++ classperson.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor (name,gender,age,place){
+        this.name=name;
+        this.gender=gender;
+        this.age=age;
+        this.place=place;
+    }
+}
+var c=new Person("Bharathi","female","26","dindigul");
+console.log(c.name);
+console.log(c.age);
